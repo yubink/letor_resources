b57c8187ee6f86cdade9e0e4d21c2241c003c503
zhuyund <zhuyund@cs.cmu.edu>
2017-07-17 01:52:19 -0400

    gather features to ranksvm format

--- scripts/letor/gather_features.py
+++ scripts/letor/gather_features.py
@@ -12,16 +12,16 @@ def init_feat_names():
                   "NA", "NA",
                   "ranks_hist", "all_hist", "NA", "redde_score", "redde_r", "redde_hist",
                   "bigram_score", "bigram_r", "bigram_hist", "prior", "title_score", "title_r", "title_hist",
-                  "url_score", "url_r", "url_hist", "inlink_score", "inlink_r", "inlink_hist",
+                  "NA", "NA", "NA", "inlink_score", "inlink_r", "inlink_hist",
                   "NA", "NA", "NA", "NA", "NA", "NA", "NA"]
     tmp = ["NA", "NA", "NA", "NA", "ctfmax", "ctfmin", "ctfidf_max", "ctfidf_min"]
 
     feat_names += ['all_' + s for s in tmp]
-    feat_names += ['url_' + s for s in tmp]
+    feat_names += ['NA' for s in tmp]
     feat_names += ['title_' + s for s in tmp]
     feat_names += ['inlink_' + s for s in tmp]
     feat_names += ["NA", "NA", "NA", "NA", "NA", "taily_score", "taily_rank"]
-    feat_names += ["m3s_{0}".format(i) for i in range(3)]
+    feat_names += ["champion_top1000", "champion_top100", "NA"]
     feat_names += ["cent_{0}".format(i) for i in range(6)]
 
     return feat_names
@@ -117,18 +117,27 @@ def read_bigram_features(qid, feat_file_path, feats, feat_names):
         feats[qid][shardid][bigram_hist_index] = r / 10
 
 
-def read_taily_score():
+def read_taily_score(taily_file_path, feats, feat_names):
+    # TODO: read taily
+    taily_score_index = feat_names.index("taily_score")
+    # read taily score for each (qid, shardid)
+    # put it into feats[qid][shardid][taily_score_index]
+
+    # TODO: get taily rank
     raise NotImplementedError
 
 
 def read_ranks_score():
     raise NotImplementedError
 
+def read_redde_score():
+    raise NotImplementedError
 
 def read_centroid_dist_score():
     raise NotImplementedError
 
 def read_champion_score():
+    # TODO read champion features: top1000 and top100
     raise NotImplementedError
 
 def to_ranksvm_format(feats, out_file_path):
@@ -162,13 +171,23 @@ if __name__ == '__main__':
     feats = {}
 
     for qid in range(1, args.n_queries + 1):
+
+        # read unigram features...
         unigram_file = "{0}/{1}.features".format(unigram_dir, qid)
         bigram_file = "{0}/{1}.features".format(bigram_dir, qid)
 
+        # read bigram features...
         read_unigram_features(qid, unigram_file, feats, feat_names)
         read_bigram_features(qid, bigram_file, feats, feat_names)
 
-        # TODO: read other features...
+        # read other features...
+        # TODO: read taily score, get reversed ranking (taily_score, taily_r)
+        # TODO: read champion features
+
+        # SLOW features
+        # read ranks score, get reversed ranking and bin_ranking (ranks_score, ranks_r, ranks_hist)
+        # read redde score, get reversed ranking and bin_ranking (redde_score, redde_r, redde_hist)
+        # read cent dist features
 
     to_ranksvm_format(feats, ranksvm_feat_file)
 
